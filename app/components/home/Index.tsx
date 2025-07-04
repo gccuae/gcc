d28304c9474f568c4454e0f33bc23a3b074bc180
rahees <raheez1995india@gmail.com>
2025-07-04 12:30:25 +0530

    test6

--- app/components/home/Index.tsx
+++ app/components/home/Index.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 
 import HeroSlider from "./HeroSlider";
 import { homeDataBanner } from "@/public/data/homebanner-data"
-/* import { homeData } from "./data"
+ import { homeData } from "./data"
 
-import { homeDataBanner,homeDataFeaturedProjects } from "@/public/data/homebanner-data"
+import {homeDataFeaturedProjects } from "@/public/data/homebanner-data"
 import AboutCompany from "./AboutCompany";
 import FeaturedProjects from "./FeaturedProjects";
 import AreaOfExpertise from "./AreaOfExpertise";
 import SectorSlider from "./SectorSlider";
-import NewsBlock from './NewsBlock'; */
+import NewsBlock from './NewsBlock'; 
 
 const Index = () => {
   return (
     <>
 
     <HeroSlider data={homeDataBanner}/>
-   {/*  
+    
        <AboutCompany />
        <FeaturedProjects data={homeDataFeaturedProjects} />
       
       <AreaOfExpertise data={homeData.fourthSection}/>
       <SectorSlider data={homeData.fifthSection} />
-      <NewsBlock title={homeData.sixthSection.title} link={homeData.sixthSection.link} items={homeData.sixthSection.items} />  */}
+      <NewsBlock title={homeData.sixthSection.title} link={homeData.sixthSection.link} items={homeData.sixthSection.items} /> 
     </>
   )
 }
