42cb736bff251273185f0d3964fc23fc00d52c64
rahees <raheez1995india@gmail.com>
2025-09-03 18:44:52 +0530

    padding

--- app/components/ServiceDetails/Main.tsx
+++ app/components/ServiceDetails/Main.tsx
@@ -5,11 +5,11 @@ const Main = () => {
         <div className="border-b border-smgray pb-8 xl:pb-[54px]">
           <h1 className="text-5xl leading-lh-text68 font-normal text-black dark:text-white">Civil & Structural Works</h1>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 xl:gap-10 justify-items-between items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 xl:gap-10 justify-items-between py-10 xl:py-20">
           <div>
             <h2 className="text-3xl leading-lh-text48 font-normal text-black dark:text-white">Building Excellence in Every Project</h2>
           </div>
-          <div className="py-10 xl:py-20">
+          <div className="">
             <p className="text-lg leading-lh-text19 text-black dark:text-white">
               <span className="text-primary">Gulf Contractors Company (GCC)</span> is a trusted name in delivering
               high-quality civil construction solutions across the UAE. With decades of experience and a team of skilled professionals, we bring innovation, precision, and safety to every project from large-scale commercial developments to complex infrastructure works.</p>
