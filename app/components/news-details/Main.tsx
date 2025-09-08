d991927be6426ee61076e5857a80b4a5f51c692d
rahees <raheez1995india@gmail.com>
2025-09-08 17:53:12 +0530

    nws detail page

---
+++ app/components/news-details/Main.tsx
@@ -0,0 +1,24 @@
+import { newsDetails } from "./data";
+import MainContent from "./MainContent";
+import SidebarContent from "./SidebarContent";
+
+const Main = () => {
+  return ( 
+    <section className="py-57px">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="border-b border-smgray pb-5 xl:pb-10">
+          <h2 className="text-5xl leading-lh-title font-normal mb-5 xl:mb-[27px] text-black dark:text-white">{newsDetails.title}</h2>
+        </div>
+        <div className="grid grid-cols-1 xl:grid-cols-[75%_25%] gap-5 xl:gap-70px mt-5 xl:mt-12">
+         
+            <MainContent subTitle={newsDetails.subTitle} image={newsDetails.image} sector={newsDetails.sector} date={newsDetails.date} desc={newsDetails.desc} />
+         
+            <SidebarContent />
+          
+        </div>
+      </div>
+    </section>
+   );
+}
+ 
+export default Main;
---
+++ app/components/news-details/MainContent.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+const MainContent = ({ subTitle, image, sector, date, desc }: { subTitle: string, image: string, sector: string, date: string, desc: string[] }) => {
+  return (
+    <div>
+      <h3 className="text-3xl leading-lh-subtitle font-normal mb-5 xl:mb-[27px] text-black dark:text-white">{subTitle}</h3>
+      <Image src={image} alt="" width={700} height={700} className="w-full h-50 object-cover" />
+      <div className="flex items-center justify-between mb-5 xl:mb-[27px] mt-3 xl:mt-[17px] gap-3">
+        <span className="text-sm">{sector}</span>
+        <span className="text-sm">{date}</span>
+      </div>
+      {desc.map((item, index) => (
+        <p key={index} className="mb-5 xl:mb-[27px] text-lg xl:text-21 leading-[1.380952380952381] font-normal">{item}</p>
+      ))}
+    </div>
+  );
+}
+
+export default MainContent;
