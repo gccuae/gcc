c2635ecda4e39853aafec725b57357e4c4015032
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-15 19:24:13 +0530

    fix(qhse page: health component): border thickness corrected

--- app/components/Qhse/sections/HealthandSafety.tsx
+++ app/components/Qhse/sections/HealthandSafety.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const HealthandSafety = ({ title, description, measures }: Props) => {
   return (
-    <section className="pt-57px bg-black">
+    <section className="py-57px bg-black">
       <div className="container">
         <div className="mb-6 xl:mb-[47px]">
           <h2 className="text-6xl leading-lh-title font-normal mb-4 xl:mb-[27px] text-white">
@@ -20,29 +20,38 @@ const HealthandSafety = ({ title, description, measures }: Props) => {
             {description}
           </p>
         </div>
-        <div className="grid grid-cols-2 lg:grid-cols-4 border border-smgray">
-          {measures.map((item, index) => (
-            <div
-              key={index}
-              className="border-r border-smgray last:border-r-0 p-[25px] lg:p-[32px] xl:p-[40px]
-               hover:bg-primary hover:text-white transition-all duration-300 group"
-            >
-              <div className="">
-                <div className="mb-[80px] xl:mb-[123px]">
-                  <Image
-                    src={item.icon}
-                    alt={item.name}
-                    width={100}
-                    height={100}
-                    className="w-10 h-auto xl:w-15 group-hover:brightness-0 group-hover:invert-100 transition-all duration-300"
-                  />
+        <div className="grid lg:grid-cols-4 border border-smgray">
+          {measures.map((item, index) => {
+            const isLastCol = (index + 1) % 4 === 0; // last col in lg grid
+            const isLastRow = index >= measures.length - 4; // last row in lg grid
+
+            return (
+              <div
+                key={index}
+                className={`
+          p-[20px] lg:p-[32px] xl:p-[35px] 2xl:p-[40px]
+          hover:bg-primary hover:text-white transition-all duration-300 group
+          ${!isLastCol ? "border-r border-smgray" : ""}
+          ${!isLastRow ? "border-b border-smgray" : ""}
+        `}
+              >
+                <div>
+                  <div className="mb-[60px] md:mb-[80px] xl:mb-[123px]">
+                    <Image
+                      src={item.icon}
+                      alt={item.name}
+                      width={50}
+                      height={50}
+                      className="w-[50px] h-[50px] xl:w-15 group-hover:brightness-0 group-hover:invert-100 transition-all duration-300"
+                    />
+                  </div>
+                  <h3 className="text-2xl leading-lh-text32 text-white group-hover:text-white transition-all duration-300">
+                    {item.name}
+                  </h3>
                 </div>
-                <h3 className="text-2xl leading-lh-text32 text-white group-hover:text-white transition-all duration-300">
-                  {item.name}
-                </h3>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
