32227ce4caa132ad029c37ff635dca571f896594
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-16 18:25:16 +0530

    feat(sustainability page): sustainability page created

--- app/components/common/ImgDesc.tsx
+++ app/components/common/ImgDesc.tsx
@@ -9,19 +9,32 @@ interface ImgDescProps {
 }
 
 const ImgDesc = ({ data }: ImgDescProps) => {
-  return ( 
+  return (
     <section className="py-57px dark:bg-black">
       <div className="container">
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:flex gap-6 xl:gap-[70px] items-center">
           <div className="xl:w-[48.6%]">
-            <Image src={data.img} alt="" width={1500} height={1500} className="w-full max-h-[772px] object-cover" />
+            <Image
+              src={data.img}
+              alt=""
+              width={1500}
+              height={1500}
+              className="w-full max-h-[772px] object-cover"
+            />
           </div>
           <div className="xl:w-[51.3%] flex flex-col gap-6 justify-center">
             <div>
-              <h2 className="text-5xl leading-[1.147058823529412] text-black dark:text-white pb-3 xl:pb-[27px] xl:tracking-[-2.1px]">{data.title}</h2>
+              <h2 className="text-5xl leading-[1.147058823529412] text-black dark:text-white pb-3 xl:pb-[27px] xl:tracking-[-2.1px]">
+                {data.title}
+              </h2>
               <div>
                 {data.desc.map((item, index) => (
-                  <p key={index} className="dark:text-white text-lg font-light leading-[1.526315789473684] mb-6 last:mb-0">{item}</p>
+                  <p
+                    key={index}
+                    className="dark:text-white text-lg font-light leading-[1.526315789473684] mb-6 last:mb-0"
+                  >
+                    {item}
+                  </p>
                 ))}
               </div>
             </div>
@@ -29,7 +42,7 @@ const ImgDesc = ({ data }: ImgDescProps) => {
         </div>
       </div>
     </section>
-   );
-}
- 
+  );
+};
+
 export default ImgDesc;
