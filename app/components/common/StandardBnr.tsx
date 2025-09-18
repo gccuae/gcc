3ab51c85e3d84515de3d8a380f81dca7f773dff9
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-18 11:58:12 +0530

    feat(animation): animation added in qhse page - 1st component

--- app/components/common/StandardBnr.tsx
+++ app/components/common/StandardBnr.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import Breadcrumb from "./BreadCrumb";
+import { motion } from "framer-motion";
 
 interface Props {
   title: string;
 }
 const StandardBnr = ({ title }: Props) => {
   return (
-
     <div className="border-b border-smgray pb-5 xl:pb-10 mb-8 xl:mb-57px">
-      <h2 className="text-5xl leading-[1.205882352941176] font-normal text-black dark:text-white">{title}</h2>
+      <motion.h2
+        initial={{ opacity: 0, x: -50, clipPath: "inset(0 100% 0 0)" }}
+        animate={{ opacity: 1, x: 0, clipPath: "inset(0 0% 0 0)" }}
+        transition={{ duration: 1.3, ease: "easeOut" }}
+        className="text-5xl leading-[1.205882352941176] font-normal text-black dark:text-white"
+      >
+        {title}
+      </motion.h2>
       <Breadcrumb standard={true} />
     </div>
   );
 };
-export default StandardBnr; 
+export default StandardBnr;
