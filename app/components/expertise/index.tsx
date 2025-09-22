8fc849d1de0549bf164442bb6cdf80e4df024870
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-22 15:47:24 +0530

    feat(.animation): implemented animation  in expertise page

--- app/components/expertise/index.tsx
+++ app/components/expertise/index.tsx
@@ -2,14 +2,18 @@ import PageBnr from "../common/PageBnr";
 import { expertiseData } from "./data";
 import ImgDesc from "../common/ImgDesc";
 import Services from "./Services";
+
 const Index = () => {
   return (
     <>
-      <PageBnr pageTitle={expertiseData.title} bannerImg={expertiseData.bannerImg} />
+      <PageBnr
+        pageTitle={expertiseData.title}
+        bannerImg={expertiseData.bannerImg}
+      />
       <ImgDesc data={expertiseData.firstSection} />
       <Services />
     </>
-   );
-}
- 
+  );
+};
+
 export default Index;
