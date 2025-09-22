0b38a4d02d434a8fff7819002dfd0c1d8cefcad2
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-22 17:58:15 +0530

    feat(animation): implemented animation in news page

--- app/components/news-listing/index.tsx
+++ app/components/news-listing/index.tsx
@@ -6,13 +6,13 @@ const Index = () => {
   const sortedNews = [...newsData.items].sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   )[0];
-  return ( 
+  return (
     <>
-    <PageBnr pageTitle="News" bannerImg="/assets/img/news/bnr.jpg" />
-    <Main title={newsData.title} link={newsData.link} items={[sortedNews]} />
-    <NewsList />
+      <PageBnr pageTitle="News" bannerImg="/assets/img/news/bnr.jpg" />
+      <Main title={newsData.title} link={newsData.link} items={[sortedNews]} />
+      <NewsList />
     </>
-   );
-}
- 
+  );
+};
+
 export default Index;
