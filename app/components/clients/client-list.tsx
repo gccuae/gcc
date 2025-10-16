756282ff23b21cdfd5611a1ec152b393772adbc1
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-16 09:06:23 +0530

    feat(api connection: clients page)

--- app/components/clients/client-list.tsx
+++ app/components/clients/client-list.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { clientsData } from "./data";
+import { ClientsFirstSection } from "./type";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { moveUp } from "../motionVarients";
 
-const ClientList = () => {
+const ClientList = ({ data }: { data: ClientsFirstSection }) => {
   return (
     <section className="py-57px">
       <div className="container">
@@ -17,7 +17,7 @@ const ClientList = () => {
             viewport={{ once: true }}
             className="text-xl leading-lh-text23 text-foreground dark:text-white/80"
           >
-            {clientsData.desc}
+            {data.description}
           </motion.p>
         </div>
         <motion.div
@@ -27,23 +27,24 @@ const ClientList = () => {
           viewport={{ once: true }}
           className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 border-l border-t border-smgray/56"
         >
-          {clientsData.clients.map((client, index) => (
+          {data.items.map((client, index) => (
             <div
               key={index}
               className="p-4 xl:p-6 flex items-center justify-center border-r border-b border-smgray/56 group h-30 xl:h-[380px] dark:bg-white"
             >
               <motion.div
-              variants={moveUp(index * 0.05)}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: true }} >
-              <Image
-                src={client}
-                alt={`client-${index + 1}`}
-                width={300}
-                height={300}
-                className="max-w-full h-full object-contain group-hover:scale-110 transition-all duration-300"
-              />
+                variants={moveUp(index * 0.05)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true }}
+              >
+                <Image
+                  src={client.logo}
+                  alt={client.logoAlt}
+                  width={300}
+                  height={300}
+                  className="max-w-full h-full object-contain group-hover:scale-110 transition-all duration-300"
+                />
               </motion.div>
             </div>
           ))}
