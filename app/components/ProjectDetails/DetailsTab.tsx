4268fd4b08ef46e25b0328f380b59d65afcc57f1
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-16 15:25:38 +0530

    fix(project-details)

--- app/components/ProjectDetails/DetailsTab.tsx
+++ app/components/ProjectDetails/DetailsTab.tsx
@@ -52,7 +52,7 @@ const DetailsTab = ({ data }: { data: ThirdSection }) => {
             viewport={{ once: true }}
             onMouseEnter={() => setIsPaused(true)}
             onMouseLeave={() => setIsPaused(false)}
-            className="relative border-y border-gray-200 dark:border-[#454545] flex flex-col md:flex-row flex-wrap space-x-6 items-baseline"
+            className="relative border-y border-gray-200 dark:border-[#454545] flex flex-col md:flex-row flex-wrap space-x-8 items-baseline"
           >
             {projectDetails.map((tab, idx) => (
               <motion.button
@@ -62,7 +62,7 @@ const DetailsTab = ({ data }: { data: ThirdSection }) => {
                 viewport={{ once: true }}
                 key={tab.title}
                 onClick={() => handleTabClick(idx)}
-                className={`py-2 md:py-4 xl:py-[27px] text-xl leading-normal font-medium relative transition-colors duration-300 px-2 hover:bg-white/50 cursor-pointer ${
+                className={`py-2 md:py-4 xl:py-[27px] text-xl leading-normal font-medium relative transition-colors duration-300 hover:bg-white/50 cursor-pointer ${
                   activeTab === idx
                     ? "text-black dark:text-white dark:text-white"
                     : "text-gray-500 dark:text-white/70 hover:text-black dark:hover:text-white dark:hover:text-white/70"
