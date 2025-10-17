b51effa012d9f47c269f981cb49871784d2d599e
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-10-17 12:57:46 +0530

    fix(ui fix - based on bug report)

--- app/components/Home/FeaturedProjects.tsx
+++ app/components/Home/FeaturedProjects.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
