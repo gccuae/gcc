6e36b892d246cbf8f14d4a67dafec46ae0e7715d
rahees <raheez1995india@gmail.com>
2026-04-06 12:25:46 +0530

    fix: adjust layout and styling in KeyFactsGrid, ProjectCard, and ProjectsList components

--- app/components/projects/sections/ProjectsList.tsx
+++ app/components/projects/sections/ProjectsList.tsx
@@ -107,12 +107,7 @@ const ProjectsList = ({
     // <section className="dark:bg-light-dark pb-37px" ref={sectionRef}>
     <section className="dark:bg-light-dark pb-12 md:pb-15 xl:pb-57px " ref={sectionRef}>
       {/* ===================== Filter Bar ===================== */}
-      <motion.div
-        variants={moveUp(0.2)}
-        initial="hidden"
-        whileInView="show"
-        className="border-b border-gray-300 dark:border-white/20 pb-27px xl:pb-0 mb-57px"
-      >
+      <motion.div variants={moveUp(0.2)} initial="hidden" whileInView="show" className="border-b border-gray-300 dark:border-white/20 pb-27px xl:pb-0 mb-57px" >
         <div className="container">
           <div className="grid grid-cols-1 2xl:grid-cols-[1fr_repeat(4,1fr)_auto] xl:grid-cols-[2fr_repeat(4,1fr)_auto] items-center gap-4 xl:gap-[30px]">
             {/* Search Input */}
